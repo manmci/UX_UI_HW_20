30954bc72c99c6574bb888a0893260381f0f14ce
manmci <88697174+manmci@users.noreply.github.com>
2022-01-19 22:53:01 -0800

    project start
    rewrote code (again), got rid of grid, moved to flex, cleaned up css, added placeholder and thumbnail images, added skeleton of webform, added dummy index files to hide file structure.

---
+++ js/js.js
@@ -0,0 +1,21 @@
+$(".chevron").on("click", function(){
+    if ($(".menuContainer").hasClass("up")) {
+        $(".menuContainer").removeClass("up").addClass("down");
+        $(".menu h2").removeClass("off");
+        $("ul").removeClass("off");
+        $(".chevron").removeClass("default").addClass("dropped");
+        $(".chevron i").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+        console.log("help... ;_;");
+    }
+    else {
+        $(".menuContainer").removeClass("down").addClass("up");
+        $(".menu h2").addClass("off");
+        $("ul").addClass("off");
+        $(".chevron").removeClass("dropped").addClass("default");
+        $(".chevron i").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    };
+});
+// $(".chevron").on("click", function(){
+//     $(".menuContainer").slideToggle(fast);
+// });
+// I'll have to change the way my CSS is set up to use slideToggle, but I think I might try it.
